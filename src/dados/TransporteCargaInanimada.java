b2346cb8f9dcb4ee5b54dd0e4733043a718b3b65
ArthurMaciel <arthurmendesmacielthuy05@gmail.com>
2024-11-22 19:35:42 -0300

    agora ss

--- src/dados/TransporteCargaInanimada.java
+++ src/dados/TransporteCargaInanimada.java
@@ -2,19 +2,19 @@ package dados;
 
 public class TransporteCargaInanimada extends Transporte {
     private boolean cargaPerigosa;
+    private Drones drone;
 
-    public TransporteCargaInanimada(int numero, String nomeCliente, String descricao, double peso, double latitudeOrigem, double latitudeDestino, double longitudeOrigem, double longitudeDestino, Estado situacao, boolean cargaPerigosa) {
+    public TransporteCargaInanimada(int numero, String nomeCliente, String descricao, double peso, double latitudeOrigem, double latitudeDestino, double longitudeOrigem, double longitudeDestino, Estado situacao, boolean cargaPerigosa, Drones drone) {
         super(numero, nomeCliente, descricao, peso, latitudeOrigem, latitudeDestino, longitudeOrigem, longitudeDestino, situacao);
         this.cargaPerigosa = cargaPerigosa;
+        this.drone = drone;
     }
 
     @Override
     public double calculaCusto() {
         double distancia = calcularDistancia();
-        double custoVariado = getDrone().calcularCustoVariado();
+        double custoVariado = drone.calcularCustoVariado();
         double acrescimo = cargaPerigosa ? 500.0 : 0.0;
         return (custoVariado * distancia) + acrescimo;
     }
-
-    // Getters e setters omitidos para brevidade
 }
--- src/dados/TransporteCargaViva.java
+++ src/dados/TransporteCargaViva.java
@@ -2,16 +2,18 @@ package dados;
 
 public class TransporteCargaViva extends Transporte {
     private double intervaloTemperatura;
+    private Drones drone;
 
-    public TransporteCargaViva(int numero, String nomeCliente, String descricao, double peso, double latitudeOrigem, double latitudeDestino, double longitudeOrigem, double longitudeDestino, Estado situacao, double intervaloTemperatura) {
+    public TransporteCargaViva(int numero, String nomeCliente, String descricao, double peso, double latitudeOrigem, double latitudeDestino, double longitudeOrigem, double longitudeDestino, Estado situacao, double intervaloTemperatura, Drones drone) {
         super(numero, nomeCliente, descricao, peso, latitudeOrigem, latitudeDestino, longitudeOrigem, longitudeDestino, situacao);
         this.intervaloTemperatura = intervaloTemperatura;
+        this.drone = drone;
     }
 
     @Override
     public double calculaCusto() {
         double distancia = calcularDistancia();
-        double custoVariado = getDrone().calcularCustoVariado();
+        double custoVariado = drone.calcularCustoVariado();
         double acrescimo = intervaloTemperatura > 10.0 ? 1000.0 : 0.0;
         return (custoVariado * distancia) + acrescimo;
     }
